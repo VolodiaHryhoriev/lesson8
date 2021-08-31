1098ec7ff76f91934ff27cb2d389984e013ac6ec
VolodiaHryhoriev <88433088+VolodiaHryhoriev@users.noreply.github.com>
2021-08-31 17:35:38 +0300

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let f1 = document.forms.f1;
 
 function moveTxt() {
-    f1.second.value = ''
+    if (f1.first.value !== '') {
+        f1.second.value = ''
+    }
     f1.second.value += f1.first.value
+    f1.first.value = ''
 }
 
 let f2 = document.forms.f2;
 
 function addPlaceholder() {
-    f2.third.placeholder = f2.third.value
-    f2.third.value = ''
+
+    if (f2.third.value !== '') {
+        f2.third.placeholder = f2.third.value
+        f2.third.value = ''
+    }
 }
